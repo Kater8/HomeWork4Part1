c511358bf7757b60f8d03fbb75ca7780420ef7f1
Kater8 <161327401+Kater8@users.noreply.github.com>
2024-03-06 00:23:00 +0200

    created the project

---
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork4Part1
+//
+//  Created by K on 06.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
