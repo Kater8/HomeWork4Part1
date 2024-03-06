99128043d99b99173a505bf834aec1c734934239
Kater8 <161327401+Kater8@users.noreply.github.com>
2024-03-06 15:51:26 +0200

    made a label

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var UILabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        UILabel.text = "Wellcome!"
     }
-
-
+ 
+    
 }
 
