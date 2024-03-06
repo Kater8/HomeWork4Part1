63833e1c21fa66f20a6f7e4757670484eabada9e
Kater8 <161327401+Kater8@users.noreply.github.com>
2024-03-06 16:23:08 +0200

    Done

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -9,14 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var UILabel: UILabel!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var yoy: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        UILabel.text = "Wellcome!"
+        label.text = "Welcome!"
+        yoy.setTitle("yoy" , for: .normal)
+        
     }
- 
+    @IBAction func button(_ sender: UIButton) {
+        sender.setTitle("yoy2", for: .normal)
+        label.text = "No Welcome!"
+    }
     
+
 }
 
